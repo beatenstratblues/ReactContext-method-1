223553b836e15358934f12d8efc597f1d1bf5370
Jatin Singh <singh.jatin609@gmail.com>
2024-07-21 08:15:58 +0530

    initial commit

---
+++ src/context/UserContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+const UserContext = createContext(); //step 1: Creating a context.
+//Every context is a providor, i.e it provides us a context variable or store, so context always gives us a providor function. These providors are often used as a wrapper to wrap components in. All the components and their further nested components that are wrapped within the providor will have access to to the context
+
+export default UserContext;
